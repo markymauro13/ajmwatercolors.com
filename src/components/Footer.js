99b231d11392b574291e4e09e1b2883f4460e6fc
Mark Mauro <markymauro@gmail.com>
2021-08-01 13:15:27 -0400

    fixing footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
         <footer className="footer--pin text-center">
             <p>Follow me for painting updates! <a href="https://www.facebook.com/tony.mauro.921"><i class="fab fa-facebook-square fa-lg ml-1"></i></a></p>
             
-            <p>Designed with 🧡 by <a href="https://www.linkedin.com/in/markalexandermauro/" className="mark">Mark Mauro</a> </p>
+          <p>Designed with 🧡 by <a href="https://www.linkedin.com/in/markalexandermauro/" className="mark">Mark Mauro</a> </p>
         </footer>
         
         </>
