98df7795c607c0e9ef665e7a77fc95042cb2c08d
Mark Mauro <markymauro@gmail.com>
2022-05-19 18:12:12 -0400

    updating navbar and about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,7 +2,6 @@ import React from 'react';
 import '../../App.css';
 import './About.css';
 import pfp from '../../images/Profile_Photos/TM_2_color.PNG';
-import Footer from '../Footer';
 
 export default function Products() {
     return (
@@ -11,13 +10,9 @@ export default function Products() {
         
         <img src={pfp} alt="tony-pfp" className="profile-photo-1"></img>
 
-        <p className="about-paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-        It has survived not only five centuries, but also the leap into electronic typesetting, 
-        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        <p className="about-paragraph">
+            Tony Mauro is a professional architect for <a href='https://kntm.com/' style={ {color: '#f7894a'}}> KNTM Architects.</a> When not designing buildings, Tony enjoys creating watercolor paintings for a variety of different sceneries. 
         </p>
-        <Footer/>
         </>
     );
 }
--- src/components/pages/Paintings.js
+++ src/components/pages/Paintings.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import '../../App.css';
 import Cards from '../Cards'
-import Footer from '../Footer';
 
 export default function Paintings() {
     return (
         <>
             <Cards /> 
-            <Footer/>
         </>
     );
 }
