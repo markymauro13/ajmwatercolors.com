9383b24e2597991580f5b54294f2a64150532a75
Mark Mauro <markymauro@gmail.com>
2022-08-30 00:17:47 -0400

    updating aj2

--- src/components/pages/Watercolors/AJ_2.js
+++ src/components/pages/Watercolors/AJ_2.js
@@ -13,7 +13,7 @@ function ProductItem() {
             <Image src={AJ_2} className="product_image" width="600" height="400"/>
             </a>
             <div className="desc-container text-center">
-                <h2>AJ 1</h2>
+                <h2>AJ J</h2>
                 <p>SOLD</p>
                 {/* <p>Dimensions: 12x16</p>
                 <div className="button-container">
