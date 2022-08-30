1dc5015c0797141f4129dbbf3c6729b5b2e1f74d
Mark Mauro <markymauro@gmail.com>
2022-08-30 00:20:19 -0400

    updating aj_1 dimensions

--- src/components/pages/Watercolors/AJ_1.js
+++ src/components/pages/Watercolors/AJ_1.js
@@ -15,7 +15,7 @@ function ProductItem() {
             <div className="desc-container text-center">
                 <h2>AJ 1</h2>
                 <p>$200</p>
-                <p>Dimensions: 12x16</p>
+                <p>Dimensions: 18x24</p>
                 <div className="button-container">
                 <a href="mailto:ajmstudiowatercolors@gmail.com?subject=Interested%20in%20AJ1&amp;body=Hello%20Tony,&amp;">
                 <Button variant="dark" type="button">Buy Now</Button>{' '}   
@@ -28,7 +28,7 @@ function ProductItem() {
             <div className="wtf">
                 <h2>AJ 2</h2>
                 <p>$200</p>
-                <p>Dimensions: 12x16</p>
+                <p>Dimensions: 18x24</p>
                 <div className="button-container">
                 <a href="mailto:ajmstudiowatercolors@gmail.com?subject=Interested%20in%20AJ1&amp;body=Hello%20Tony,&amp;">
                 <Button variant="dark" type="button">Buy Now</Button>{' '}   
